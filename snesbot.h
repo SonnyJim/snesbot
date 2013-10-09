240880b7ade1a2b778d3c5bfc31399d7c7d4279c
SonnyJim <ewan.meadows@gmail.com>
2013-10-09 05:33:19 +0100

    Add header file

---
+++ snesbot.h
@@ -0,0 +1,22 @@
+// 			WiringPi	P1 Pin
+#define B_Pin		 8 		// 3
+#define Y_Pin		 9 		// 5
+#define Select_Pin	 7 		// 7
+#define Start_Pin	 15 		// 8
+#define Up_Pin		 16 		// 10
+#define Down_Pin	 0 		// 11
+#define Left_Pin	 1 		// 12
+#define Right_Pin	 2 		// 13
+#define A_Pin		 3 		// 15
+#define X_Pin		 4 		// 16
+#define TLeft_Pin	 5 		// 18
+#define TRight_Pin	 12 		// 19
+
+#define Latch_Pin	 13 		// 21
+
+#define RECBUFSIZE	1024 * 16 //Set record buffer to 16MB
+
+//How many SNES buttons there are
+#define NUMBUTTONS	12
+
+
